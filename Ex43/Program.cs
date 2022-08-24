d715a4d6560c4f06797e8b05bddf2ea0d5de646a
Ekaterina <katya.kinal@gmail.com>
2022-08-24 21:50:26 +0300

    Ex43

---
+++ Ex43/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int digit = -1;
+int result = number;
+int[] array = new int[10];
+
+while(result > 0)
+{
+digit = result % 2;
+array[i] = digit;
+result /= 2;
+i++;
+}
+
+for(int ind = i; ind > 0; ind--)
+{
+while(array[i] != 1)
+{
+i--;
+}
+Console.Write(array[ind-1]);
+}
