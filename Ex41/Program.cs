0438a262afe3391d1fb5af26d29759fa1d693c63
Ekaterina <katya.kinal@gmail.com>
2022-08-24 21:44:08 +0300

    Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arrayM = new int[8];
+for(int i = 0; i < arrayM.Length; i++)
+{
+Console.WriteLine($"Введите {i} элемент массива: ");
+arrayM[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for(int i = 0; i < arrayM.Length; i++)
+{
+if(arrayM[i] > 0) count += 1;
+}
+
+Console.Write($"{String.Join (", ", arrayM)} -> {count}");
